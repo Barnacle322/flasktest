98dc3a1390ba9dde25cd215ea9a6ff071e1f2f15
Your NameBarnacle322 <you@example.comarstan.usenov@gmail.com>
2022-04-28 15:20:57 +0200

    new commit

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ class Item(db.Model):
     price = db.Column(db.Float, nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
     complete = db.Column(db.Boolean, default = False)
-    editable =db.Column(db.Boolean, default = False)
+    editable = db.Column(db.Boolean, default = False)
+    author = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return '<Item %r>' % self.name
@@ -35,13 +36,17 @@ class House(db.Model):
     name = db.Column(db.String(80), nullable=False)
     description = db.Column(db.String(120))
 
-class Table(db.Model):
+class Item_House_Map(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     house_id = db.Column(db.Integer, db.ForeignKey('house.id'), nullable=False)
+    item_id = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=True)
+
+class User_House_Map(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    house_id = db.Column(db.Integer, db.ForeignKey('house.id'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    item_id = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=False)
-
-
+    accepted = db.Column(db.Boolean, default = False)
 
 db.create_all()
 
@@ -51,6 +56,25 @@ def before_request():
 
     if 'user_id' in session:
         g.user = db.session.query(User).filter(User.id == session['user_id']).first()
+
+@app.post('/invite_user/<int:house_id>/<int:user_id>')
+def invite_user(house_id, user_id):
+    house_id = house_id
+    user_id = user_id
+
+    new_table = User_House_Map(sender = g.user.id, house_id = house_id, user_id=user_id)
+    db.session.add(new_table)
+    db.session.commit()
+    return redirect("/tracker/" + str(house_id))
+
+
+@app.get("/user_list/<int:house_id>")
+def user_list(house_id):
+    if not g.user:
+        return redirect(url_for('login'))
+    table = db.session.query(User_House_Map).filter(User_House_Map.house_id == house_id).first()
+    user_list = db.session.query(User).filter(User.id != g.user.id).filter(User.id != table.user_id).all()
+    return render_template('userlist.html', user_list=user_list, house_id=house_id)
 
 # Laod the home page
 @app.get("/")
@@ -70,13 +94,13 @@ def add_item(house_id):
 
     name = request.form.get("name")
     price = request.form.get("price")
-    quantity = request.form.get("quantity")
-    new_item = Item(name=name, price=price, quantity=quantity)
+    quantity = request.form.get("quantity")   
+    user_id = g.user.id
+    new_item = Item(name=name, price=price, quantity=quantity, author=user_id)
     db.session.add(new_item)    
     db.session.commit()
 
-    item = db.session.query(Item).filter(Item.name == name, Item.price == price, Item.quantity == quantity).first()
-    new_table = Table(house_id = house_id, user_id=g.user.id, item_id=item.id)
+    new_table = Item_House_Map(house_id=house_id, item_id=new_item.id)
     db.session.add(new_table)
     db.session.commit()
     return redirect("/tracker/" + str(house_id))
@@ -93,6 +117,9 @@ def add_house():
         house = House(user_id = g.user.id, name = name, description = description)
         db.session.add(house)
         db.session.commit()
+        table = User_House_Map(sender = g.user.id, user_id = g.user.id, house_id = house.id, accepted = True)    
+        db.session.add(table) 
+        db.session.commit()
         return redirect(url_for("profile"))
         
     return render_template('add_house.html')
@@ -104,24 +131,46 @@ def delete_house(house_id):
     db.session.commit()
     return redirect(url_for("profile"))
 
-@app.get("/user_list")
-def user_list():
-    if not g.user:
-        return redirect(url_for('login'))
-
-    user_list = db.session.query(User).all()
-    return render_template('userlist.html', user_list=user_list)
-
 @app.get("/profile")
 def profile():
     if not g.user:
         return redirect(url_for('login'))
+
     try:
-        house_list = db.session.query(House).filter(House.user_id == g.user.id).all()
+        table_list = db.session.query(User_House_Map).filter(User_House_Map.user_id == g.user.id).filter(User_House_Map.accepted == True).all()
+        house_list = [db.session.query(House).filter(House.id == table.house_id).first() for table in table_list if table.house_id != None]
     except:
         house_list = []
 
     return render_template('profile.html', house_list = house_list)
+
+@app.get("/notifications")
+def notifications():
+    if not g.user:
+        return redirect(url_for('login'))
+
+    try:
+        # Get the user_house_map table for the logged user to get all unaccepted invites.
+        table_list = db.session.query(User_House_Map).filter(User_House_Map.user_id == g.user.id).filter(User_House_Map.accepted == False).all()
+        # Swap the sender id with the senders username.
+        for element in table_list:
+            user = db.session.query(User).filter(User.id == element.sender).first()
+            house = db.session.query(House).filter(House.id == element.house_id).first()
+            element.sender = user.username
+            element.house_name = house.name
+
+        house_list = [db.session.query(House).filter(House.id == table.house_id).first() for table in table_list if table.house_id != None]
+    except:
+        house_list = []
+        table_list = []
+    return render_template('notifications.html', house_list = house_list, table_list = table_list)
+
+@app.get("/accept_invitation/<int:table_id>")
+def accept_invitation(table_id):
+    table = db.session.query(User_House_Map).filter(User_House_Map.id == table_id).first()
+    table.accepted = True
+    db.session.commit()
+    return redirect(url_for("notifications"))
 
 # Add a new user
 @app.post("/users")
@@ -145,7 +194,7 @@ def update(item_id):
 @app.get("/delete/<int:item_id>")
 def delete(item_id):
     item = db.session.query(Item).filter(Item.id == item_id).first()
-    table = db.session.query(Table).filter(Table.item_id == item_id).first()
+    table = db.session.query(Item_House_Map).filter(Item_House_Map.item_id == item_id).first()
     db.session.delete(table)
     db.session.delete(item)
     db.session.commit()
@@ -199,13 +248,27 @@ def logout():
 def tracker(house_id):
     if not g.user:
         return redirect(url_for('login'))
-    table_list = db.session.query(Table).filter(Table.user_id == g.user.id).filter(Table.house_id == house_id).all()
+    house = db.session.query(User_House_Map).filter(User_House_Map.user_id == g.user.id).filter(User_House_Map.house_id == house_id).first()
+    if house == None:
+        return redirect(url_for('profile'))
+
+    # Queries the "Table" to get to get all the item ids that are compliant with user_id and house_id
+    table_list = db.session.query(Item_House_Map).filter(Item_House_Map.house_id == house_id).all()
     # list = []
 
     # for table in table_list:
     #     list.append(db.session.query(Item).filter(Item.id == table.item_id).first())
+
+    # Get all the items from the table Item that are compliant with the coresponding item id in the table id, query for which was preceding.
     list = [db.session.query(Item).filter(Item.id == table.item_id).first() for table in table_list if table.item_id]
 
+    for element in list:
+        username = db.session.query(User.username).filter(User.id == element.author).first()
+        try:
+            element.author = username[0]
+        except:
+            element.author = username
+    
     return render_template('tracker.html', item_list = list, house_id = house_id)
 
 if __name__ == "__main__":
